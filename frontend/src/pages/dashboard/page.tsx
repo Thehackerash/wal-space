28a04bbc828751d054e0efdd9a089874b114a2fd
devAmaresh <amprasad.2003@gmail.com>
2024-08-04 14:05:16 +0530

    done sidebar

--- frontend/src/pages/dashboard/page.tsx
+++ frontend/src/pages/dashboard/page.tsx
@@ -1,13 +1,25 @@
-import { Button } from "antd";
+
+import { BiLogOut } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 const page = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <div>Dashboard</div>
-      <Button type="dashed" href="/logout">
-        {" "}
-        Logout
-      </Button>
+      <div className="grid grid-cols-12">
+        <div className="col-span-3">
+          <div
+            className="p-5 flex justify-center hover:text-zinc-500 cursor-pointer"
+            onClick={() => {
+              navigate("/logout");
+            }}
+          >
+            <BiLogOut className=" mr-2 w-7 h-7" /> Logout
+          </div>
+        </div>
+
+        <div className="col-span-9">WELCOME TO THE DASHBOARD</div>
+      </div>
     </>
   );
 };
