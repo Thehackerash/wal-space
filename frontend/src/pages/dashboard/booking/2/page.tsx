446aa0d8262785326e071a335f08648b0ae3ed4e
devAmaresh <amprasad.2003@gmail.com>
2024-08-11 12:30:02 +0530

    fix

--- frontend/src/pages/dashboard/booking/2/page.tsx
+++ frontend/src/pages/dashboard/booking/2/page.tsx
@@ -1,36 +1,36 @@
-import { Map as MapLibreMap, NavigationControl } from "maplibre-gl";
+import DeckGL from "@deck.gl/react";
+import StaticMap from "react-map-gl";
+import maplibregl, { NavigationControl } from "maplibre-gl";
+
 import "maplibre-gl/dist/maplibre-gl.css";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const page = () => {
-    const [mapReady, setMapReady] = useState(false);
-
-    useEffect(() => {
-      if (!mapReady) return;
-
-      const map = new MapLibreMap({
-        container: "central-map",
-        center: [0, 0],
-        zoom: 0,
-        style: "https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json",
-        transformRequest: (url, resourceType) => {
-          url = url + `?api_key=${import.meta.env.VITE_BASE_MAP_API_KEY}`;
-          return { url, resourceType };
-        },
-      });
-
-      const nav = new NavigationControl({
-        visualizePitch: true,
-      });
-      map.addControl(nav, "top-left");
-    }, [mapReady]);
+  const [viewState, setViewState] = useState({
+    longitude: 0,
+    latitude: 0,
+    zoom: 1,
+  });
 
         return (
-          <div
+          <div>
+          <DeckGL
             style={{ width: "100vw", height: "100vh", overflow: "hidden" }}
-            ref={() => setMapReady(true)}
-            id="central-map"
-          />
+            viewState={viewState}
+            onViewStateChange={({ viewState }:any) => setViewState(viewState)}
+            controller={true}
+            layers={[]}
+          >
+            <StaticMap
+              mapLib={maplibregl as any}
+              mapStyle="https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json"
+              transformRequest={(url, resourceType) => {
+                url = url + `?api_key=${import.meta.env.VITE_BASE_MAP_API_KEY}}`;
+                return { url, resourceType };
+              }}
+            />
+          </DeckGL>
+        </div>
         );
 };
 
